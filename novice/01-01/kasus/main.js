dfbc5a0d02fe132c17f35e3f58945e6407305a28
novieananda <novie.ananda@students.amikom.ac.id>
2021-02-08 15:01:29 +0700

    pertemuan 1

---
+++ novice/01-01/kasus/main.js
@@ -0,0 +1,3 @@
+let dataDiri = "Nama : Novie Ananda <p> Alamat : Kulon Progo <p> Pendidikan : S1 Informatika Transfer";
+console.log(dataDiri);
+document.getElementById("nana").innerHTML = dataDiri;
---
+++ novice/01-01/latihan/main.js
@@ -0,0 +1,25 @@
+//Hello world
+// const myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Hello world!';
+
+//variables
+// let myVariable = "Bob";
+// myVariable = "Steve";
+// console.log(myVariable);
+
+//conditions
+// let iceCream = 'chocolate';
+// if (iceCream === 'chocolate') {
+//     alert('Yay, I love chocolate ice cream!');
+// } else {
+//     alert('Awwww, but chocolate is my favorite...');
+// }
+
+// function multiply(num1, num2) {
+//     let result = num1 * num2;
+//     return result;
+// }
+
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
