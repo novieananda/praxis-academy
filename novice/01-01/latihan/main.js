32df367ac0624e5abf72fa40103c077ae06b57ae
novieananda <novie.ananda@students.amikom.ac.id>
2021-02-09 13:44:01 +0700

    pertemuan 2

--- novice/01-01/latihan/main.js
+++ novice/01-01/latihan/main.js
@@ -1,13 +1,13 @@
-//Hello world
+// Hello world
 // const myHeading = document.querySelector('h1');
 // myHeading.textContent = 'Hello world!';
 
-//variables
+// variables
 // let myVariable = "Bob";
 // myVariable = "Steve";
 // console.log(myVariable);
 
-//conditions
+// conditions
 // let iceCream = 'chocolate';
 // if (iceCream === 'chocolate') {
 //     alert('Yay, I love chocolate ice cream!');
@@ -15,11 +15,46 @@
 //     alert('Awwww, but chocolate is my favorite...');
 // }
 
+// document.querySelector('html').onclick = function() {
+//     alert('Ouch! Stop poking me!');
+// }
+
+//functions
+// let myVariable = document.querySelector('h1');
+// alert('hello!');
+
 // function multiply(num1, num2) {
 //     let result = num1 * num2;
 //     return result;
-// }
+//}
 
-document.querySelector('html').onclick = function() {
-    alert('Ouch! Stop poking me!');
+//img
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'download.jpg') {
+        myImage.setAttribute('src', 'download.jpg');
+    } else {
+        myImage.setAttribute('src', 'downloa.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function() {
+    setUserName();
 }
---
+++ novice/01-02/latihan/conditional statements/main.js
@@ -0,0 +1,11 @@
+function checkData() {
+    if (document.form1.threeChar.value.length == 3) {
+        return true;
+    } else {
+        alert(
+            'Enter exactly three characters. ' +
+            `${document.form1.threeChar.value} is not valid.`);
+        return false;
+    }
+}
+console.log(checkData);
